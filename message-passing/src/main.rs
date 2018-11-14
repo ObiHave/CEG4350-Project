943113ef3650e1d0e34b1a8b5091e88ca54cd62d
Cheney <sollenberger.15@wright.edu>
2018-11-14 17:24:29 -0500

    Optimization of the message passing

--- message-passing/src/main.rs
+++ message-passing/src/main.rs
@@ -9,19 +9,22 @@ fn main() {
     let mut range = thread_rng();
     // Create a channel for message passing.
     let (tx, rx) = mpsc::channel();
-      
+    // Generates a Vector of 100 random unsigned integers
+    let mut vals: Vec<u8> = (0..100).map(|_| range.gen_range(0,100)).collect();
+    
+    // Spawn a thread to recieve the message and print it to the terminal
     thread::spawn(move || {
         for recieved in rx {
             println!("Recieved: {}", recieved);
         }   
     });
     // Generate and send the random produced numbers
-    let mut i = 0;
-    while i != 100 {
-        let x: u8 = range.gen_range(0,100);
+    for _ in 0..100 {
+        // Pop the last item in the random data vector
+        let x: u8 = vals.pop().unwrap();
+        // Print the data to the terminal and send it to the receiver
         print!("Produced: {}\t", x);
         tx.send(x).unwrap();
-        i = i + 1;
         thread::sleep(Duration::from_millis(1000));
     }
     println!("\nDone.");
---
+++ message-passing/target/debug/deps/rand_core-acf7e645337b9c6f.d
@@ -0,0 +1,9 @@
+/home/cheney/Desktop/CEG4350-Project/message-passing/target/debug/deps/librand_core-acf7e645337b9c6f.rlib: /home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/lib.rs /home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/error.rs /home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/block.rs /home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/impls.rs /home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/le.rs
+
+/home/cheney/Desktop/CEG4350-Project/message-passing/target/debug/deps/rand_core-acf7e645337b9c6f.d: /home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/lib.rs /home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/error.rs /home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/block.rs /home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/impls.rs /home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/le.rs
+
+/home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/lib.rs:
+/home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/error.rs:
+/home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/block.rs:
+/home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/impls.rs:
+/home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/le.rs:
