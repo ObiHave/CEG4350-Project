87020f4426680060c0a9f45fc635b2f92b17ed29
Cheney <sollenberger.15@wright.edu>
2018-11-14 18:36:02 -0500

    Sockets are getting closer

--- socket/src/main.rs
+++ socket/src/main.rs
@@ -4,33 +4,29 @@ use std::thread;
 use std::net::UdpSocket;
 use std::time::Duration;
 
-fn main() {
+fn main() {    
     // Create a range for the random number generator
     let mut range = thread_rng();
-    // Bind the socket, localhost port 8080
-    let socket = UdpSocket::bind("127.0.0.0.1::8080").unwrap();
-    println!("Listening on {}", socket.local_addr().unwrap());
+    // Bind the sockets, localhost ports 8080 and 8081
+    let mut producer_socket = UdpSocket::bind("127.0.0.1:8080").expect("Couldn't bind producer socket.");
+    let mut consumer_socket = UdpSocket::bind("127.0.0.1:8081").expect("Couldn't bind consumer socket.");
+    let prod_name = producer_socket.local_addr().unwrap();
+    let cons_name = consumer_socket.local_addr().unwrap();
 
     //Create a buffer to hold the data sent/received
     let mut buffer = [0, 100];
 
-    thread::spawn(move || {
-         //Receive the data over the socket
-         let (size, peer) = socket.recv_from(&mut buffer).expect("Failed to receive message.");
-         for i in 0..size {
-             println!("Received: {}", buffer[i]);
-         }
-    });
-    // Generate and send the random produced numbers
-    let mut i = 0;
+    // Generates a Vector of 100 random unsigned integers
+    let vals: Vec<u8> = (0..100).map(|_| range.gen()).collect();
 
-    while i != 100 {
-        let vals: Vec<u8> = (0..100).map(|_| range.gen_range(0,100)).collect();
-      //  print!("Produced: {}\t", vals);
+    thread::spawn(move || {
         // Send the data over the socket
-        let _amt = socket.send_to(&vals.as_slice(), socket.local_addr().unwrap());
-        i = i + 1;
-        thread::sleep(Duration::from_millis(1000));
-    }
+        let amt = producer_socket.send_to(&vals[..], cons_name);
+        println!("{}: Sent {} bytes to {}.", prod_name, amt.unwrap(), cons_name);
+    });
+   // Generate and send the random produced numbers
+    //Receive the data over the socket
+    let (size, peer) = consumer_socket.recv_from(&mut buffer).expect("Failed to receive message.");
+    println!("{}: Received {} bytes from {}", cons_name, size, peer);
     println!("\nDone.");
 }
---
+++ socket/target/debug/deps/rand_core-7a7c02cedfa00929.d
@@ -0,0 +1,5 @@
+/home/cheney/Desktop/CEG4350-Project/socket/target/debug/deps/librand_core-7a7c02cedfa00929.rlib: /home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.2/src/lib.rs
+
+/home/cheney/Desktop/CEG4350-Project/socket/target/debug/deps/rand_core-7a7c02cedfa00929.d: /home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.2/src/lib.rs
+
+/home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.2/src/lib.rs:
