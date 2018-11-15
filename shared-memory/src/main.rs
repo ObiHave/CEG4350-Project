f5b84c8af26aa8e6ca7001d35f65496159ce7ba7
Cheney <sollenberger.15@wright.edu>
2018-11-15 12:37:24 -0800

    Attemptinng more optimization

--- shared-memory/src/main.rs
+++ shared-memory/src/main.rs
@@ -1,14 +1,14 @@
+extern crate circular_queue;
 extern crate rand;
 use rand::prelude::*;
 use std::thread;
 use std::sync::{Mutex, Arc};
 use std::time::Duration;
+use circular_queue::CircularQueue;
 
 fn main() {
     // Create a thread safe Mutex value initialized to 0.
-    let val = Arc::new(Mutex::new(0));    
-    // Create a range for the random number generator
-    let mut range = thread_rng();
+    let val = Arc::new(Mutex::new(0));
     for _ in 0..100 {
         let mutex = Arc::clone(&val);
         thread::spawn(move || {
@@ -22,7 +22,6 @@ fn main() {
         // Assign the value to a random generated value.
         *tx = rand::random::<u8>();
         print!("Produced: {}\t", *tx);
-        thread::sleep(Duration::from_millis(250));
     }
     println!("\nDone.");
 }
---
+++ shared-memory/target/debug/deps/circular_queue-1fe087f632d617a3.d
@@ -0,0 +1,5 @@
+/home/cheney/Desktop/CEG4350-Project/shared-memory/target/debug/deps/libcircular_queue-1fe087f632d617a3.rlib: /home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/circular-queue-0.2.0/src/lib.rs
+
+/home/cheney/Desktop/CEG4350-Project/shared-memory/target/debug/deps/circular_queue-1fe087f632d617a3.d: /home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/circular-queue-0.2.0/src/lib.rs
+
+/home/cheney/.cargo/registry/src/github.com-1ecc6299db9ec823/circular-queue-0.2.0/src/lib.rs:
