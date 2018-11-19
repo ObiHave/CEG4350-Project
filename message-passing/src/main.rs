7123d9d0ebde621c101cab94d945daa22e44e8f5
Cheney <sollenberger.15@wright.edu>
2018-11-19 14:42:15 -0500

    Optimized and formatted.

--- message-passing/src/main.rs
+++ message-passing/src/main.rs
@@ -1,16 +1,13 @@
 extern crate rand;
-use rand::prelude::*;
 use std::thread;
 use std::sync::mpsc;
 use std::time::Duration;
 
 fn main() {
-    // Create a range for the random number generator
-    let mut range = thread_rng();
     // Create a channel for message passing.
     let (tx, rx) = mpsc::channel();
     // Generates a Vector of 100 random unsigned integers
-    let mut vals: Vec<u8> = (0..100).map(|_| range.gen()).collect();
+    let mut vals: Vec<u8> = (0..100).map(|_| rand::random::<u8>()).collect();
     
     // Spawn a thread to recieve the message and print it to the terminal
     thread::spawn(move || {
@@ -18,16 +15,19 @@ fn main() {
             println!("Recieved: {}", recieved);
         }   
     });
-    // Generate and send the random produced numbers
-    for _ in 0..100 {
-        // Pop the last item off the random data vector
-        let x: u8 = vals.pop().unwrap();
-        // Print the data to the terminal
-        print!("Produced: {}\t", x);
-        // Send the data to the reciever
-        tx.send(x).unwrap();
+    let _ = thread::spawn(move || {
+        for _ in 0..100 {
+            // Pop the last item off the random data vector
+            let x: u8 = vals.pop().unwrap();
+            // Print the data to the terminal
+            print!("Produced: {}\t", x);
+            // Send the data to the reciever
+            tx.send(x).unwrap();
 
-        thread::sleep(Duration::from_millis(250));
-    }
+            thread::sleep(Duration::from_millis(250));
+        }
+    }).join();
+    // Generate and send the random produced numbers
+    
     println!("\nDone.");
 }
